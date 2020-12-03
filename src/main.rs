db5af04fcb9dfd656d8727da37dec9e3443230ae
Constantine Theocharis <cthe@mailbox.org>
2020-12-03 17:51:59 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::{
+    env,
+    process::{exit, Command},
+};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SpaceQueryEntry {
+    index: i32,
+    focused: i32,
+}
+
+fn main() {
+    let args = env::args().collect::<Vec<_>>();
+
+    let usage = || -> ! {
+        eprintln!("Usage: has_space_rs {{next|prev}}");
+        exit(1);
+    };
+
+    let change: isize = if args.len() != 2 {
+        usage();
+    } else if args[1] == "next" {
+        1
+    } else if args[1] == "prev" {
+        -1
+    } else {
+        usage();
+    };
+
+    let stdout = Command::new("yabai")
+        .args(&["-m", "query", "--spaces", "--display"])
+        .output()
+        .expect("Failed to execute command")
+        .stdout;
+
+    let spaces: Vec<SpaceQueryEntry> = serde_json::from_slice(&stdout).expect("Got invalid JSON");
+
+    let current = spaces
+            .iter()
+            .enumerate()
+            .find_map(|(i, x)| if x.focused == 1 { Some(i as isize) } else { None })
+            .expect("Was expecting to find one space which is focused");
+
+    let next = current + change;
+    if next >= 0 && next < spaces.len() as isize {
+        exit(0);
+    } else {
+        exit(1);
+    }
+}
